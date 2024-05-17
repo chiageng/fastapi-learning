bd631f5b74b397df220cdf4659d29bbff1f979f9
chiageng <chiageng12@gmail.com>
2024-05-17 20:03:14 +0800

    Database change to render

--- database.py
+++ database.py
@@ -3,7 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:062831@localhost/TodoApplicationDatabase"
+# DATABASE_URL = "postgresql://postgres:062831@localhost/TodoApplicationDatabase"
+
+## Render database url
+DATABASE_URL = "postgresql://todosapplicationdatabase_lwxu_user:TyXkaa0v8fGnJRCbivoCk0GvvYzpuxXF@dpg-cp3k9gvsc6pc73fqli30-a.singapore-postgres.render.com/todosapplicationdatabase_lwxu"
 
 # Create the synchronous engine
 engine = create_engine(DATABASE_URL, echo=True)
